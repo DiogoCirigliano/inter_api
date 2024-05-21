617db9730ce83f6e8781e869ab98d2b330aaade5
DiogoCirigliano <devsdi2006@gmail.com>
2024-05-21 14:50:45 -0300

    Implementa a funcionalidade de autenticação do token de acesso em várias classes relevantes para integração com a API de banking.

---
+++ inter/inter/commons/ConfigSetup.cs
@@ -0,0 +1,19 @@
+﻿namespace sdk
+{
+    class ConfigSetup
+    {
+        static void Main(string[] args)
+        {
+            Config config = Config.Builder()
+                .SetAmbiente("production")
+                .SetClientId("your-client-id")
+                .SetClientSecret("your-client-secret")
+                .SetCertificado("path/to/certificate")
+                .SetSenha("your-certificate-password")
+                .SetDebug(true)
+                .SetContaCorrente("1234567890")
+                .SetControleRateLimit(true)
+                .Build();
+        }
+    }
+}
---
+++ inter/inter/sdk/Constants.cs
@@ -0,0 +1,25 @@
+﻿namespace sdk
+{
+    public static class Constants
+    {
+        public const string URL_BASE = "https://cdpj.partners.AMBIENTE.com.br";
+        public const string URL_TOKEN = URL_BASE + "/oauth/v2/token";
+        public const string CALLBACKS = "callbacks";
+        public const string URL_BANKING_SALDO = URL_BASE + "/banking/v2/saldo";
+        public const string URL_BANKING_EXTRATO = URL_BASE + "/banking/v2/extrato";
+        public const string URL_BANKING_EXTRATO_ENRIQUECIDO = URL_BASE + "/banking/v2/extrato/completo";
+        public const string URL_BANKING_EXTRATO_PDF = URL_BASE + "/banking/v2/extrato/exportar";
+        public const string URL_BANKING_WEBHOOK = URL_BASE + "/banking/v2/webhooks";
+        public const string DOC_CERTIFICADO = "https://developers.bancointer.com.br/v4/docs/onde-obter-o-certificado";
+        public const string ESCOPO_EXTRATO_READ = "extrato.read";
+        public const string ESCOPO_LOCATION_READ = "payloadlocation.read";
+        public const string ESCOPO_LOCATION_WRITE = "payloadlocation.write";
+        public const string ESCOPO_WEBHOOK_READ = "webhook.read";
+        public const string ESCOPO_WEBHOOK_WRITE = "webhook.write";
+        public const string ESCOPO_WEBHOOK_BANKING_READ = "webhook-banking.read";
+        public const string ESCOPO_WEBHOOK_BANKING_WRITE = "webhook-banking.write";
+        public const int DAYS_TO_EXPIRE = 30;
+        public const string CERTIFICATE_EXCEPTION_MESSAGE = "Erro no Certificado!";
+        public const string GENERIC_EXCEPTION_MESSAGE = "Erro durante execução do SDK!";
+    }
+}
